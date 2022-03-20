904d193eebc26f5a63aaee2b282737ef568aab8a
Charlotte Buchan <cebuc@talktalk.net>
2022-03-20 13:45:11 +0000

    added index.html for home page

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ app.listen(3000, () => {
   console.log('listening on 3000')
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World')
+// });
 
 app.get('/', (req, res) => {
-  res.sendFile('/Users/charlottebuchan/Projects/week-8/instagram-challenge' + '/index.html')
+  res.sendFile('/Users/charlottebuchan/Projects/week-8/instagram-challenge' + '/views/home/index.html')
   // Note: __dirname is the current directory you're in. Try logging it and see what you get!
   // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
 })
