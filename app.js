30992d5b53eb02939514e15f3fd0a4cb80a3bec5
Charlotte Buchan <cebuc@talktalk.net>
2022-03-20 12:52:53 +0000

    installed nondemon

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const app = express();
 
 app.listen(3000, () => {
   console.log('listening on 3000')
-})
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World')
+});
+
+app.get('/', (req, res) => {
+  res.sendFile('/Users/charlottebuchan/Projects/week-8/instagram-challenge' + '/index.html')
+  // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
 })
 
 console.log('This is a test message');
